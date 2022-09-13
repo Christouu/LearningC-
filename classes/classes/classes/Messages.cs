899c1cb4ce2ccd8e8777b67006b1c0e724e41484
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-09-13 12:04:25 +0300

    classes in c#

---
+++ classes/classes/classes/Messages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("waiting..");
+        }
+
+        public static void Bye()
+        {
+            Console.WriteLine("Bye");
+        }
+    }
+}
