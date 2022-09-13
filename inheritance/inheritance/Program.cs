2ac7c231615a13708353d1a14aa1ec2b72f95f0d
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-09-13 13:10:18 +0300

    inheritance in C#

---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Inheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            Bike bike = new Bike();
+            Boat boat = new Boat();
+
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            car.Go();
+
+            Console.WriteLine(bike.speed);
+            Console.WriteLine(bike.wheels);
+            bike.Go();
+
+            Console.WriteLine(boat.speed);
+            Console.WriteLine(boat.wheels);
+            boat.Go();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Vehicle
+    {
+        public int speed = 0;
+
+        public void Go()
+        {
+            Console.WriteLine("This vehicle is moving");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+    }
+
+    class Bike : Vehicle
+    {
+        public int wheels = 2;
+    }
+
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+    }
+}
