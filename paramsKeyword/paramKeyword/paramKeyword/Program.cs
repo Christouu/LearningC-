9cb86156bf23ad139fd22c42a3ce933728c7ee1f
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-09-13 11:13:01 +0300

    params keyword in C#

---
+++ paramsKeyword/paramKeyword/paramKeyword/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double total = CheckOut(3.99, 1.23, 2.5);
+
+
+        Console.WriteLine(total);
+        Console.ReadKey();
+    }
+
+    static double CheckOut(params double[] prices)
+    {
+        double total = 0;
+
+        foreach (double price in prices)
+
+        {
+            total += price;
+        };
+
+        return total;
+    }
+}
