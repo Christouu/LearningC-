0c1c378eedd110f52b507f0ac9ef8adfcd319105
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-09-13 12:55:04 +0300

    overloaded constructors technique

---
+++ overloadedConstructors/overloadedConstructor/overloadedConstructor/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        Pizza pizza1 = new Pizza("white", "tomato", "cream", "mozzarella");
+
+        Console.ReadKey();
+    }
+}
+
+class Pizza
+{
+    string bread;
+    string sauce;
+    string cheese;
+    string topping;
+
+
+    public Pizza(string bread, string sauce)
+    {
+        this.bread = bread;
+        this.sauce = sauce;
+
+    }
+
+    public Pizza(string bread, string cheese, string topping)
+    {
+        this.bread = bread;
+        this.cheese = cheese;
+        this.topping = topping;
+    }
+
+    public Pizza(string bread, string sauce, string cheese, string topping)
+    {
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+    }
+}
