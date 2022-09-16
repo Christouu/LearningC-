9a21d0cb1fa4d8884f50e7a4668581c5d934b7e2
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-09-16 12:39:05 +0300

    conditionals

---
+++ conditinals/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,34 @@
+﻿var seconds = DateTime.Now.Second;
+
+Console.WriteLine("Current seconds: " + seconds);
+
+/*
+if (seconds % 2 == 0)
+{
+    Console.WriteLine("Seconds are even");
+}
+else if (seconds % 3 == 0)
+{
+    Console.WriteLine("Seconds are a multiple of number 3");
+}
+else
+{
+    Console.WriteLine("Seconds are neither even nor a multiple of 3");
+}
+*/
+
+
+
+// OR | , AND & 
+if (seconds % 2 == 0 & seconds % 3 == 0)
+{
+    Console.WriteLine("Seconds are even AND a multiple of 3");
+}
+else if (seconds % 2 == 0)
+{
+    Console.WriteLine("Seconds are even");
+}
+else if (seconds % 3 == 0)
+{
+    Console.WriteLine("Seconds are not even");
+}
---
+++ conditinals/Conditionals/Conditionals/switch.cs
@@ -0,0 +1,32 @@
+﻿class DayOfTheWeek
+{
+
+    DayOfWeek dayOfTheWeek = DateTime.Now.DayOfWeek;
+    int hourOfDay = DateTime.Now.Hour;
+
+    public void WhatDayItIs(DayOfWeek day, int hour)
+    {
+        switch (day)
+        {
+            case DayOfWeek.Monday:
+            case DayOfWeek.Tuesday:
+            case DayOfWeek.Wednesday:
+            case DayOfWeek.Thursday:
+            case DayOfWeek.Friday when hour < 16:
+                Console.WriteLine("Work work work...");
+                break;
+            case DayOfWeek.Friday when hour >= 16:
+                Console.WriteLine("The party starts now");
+                break;
+            case DayOfWeek.Saturday:
+            case DayOfWeek.Sunday:
+                Console.WriteLine("Chill");
+                break;
+        }
+
+
+        WhatDayItIs(dayOfTheWeek, hourOfDay);
+    }
+
+
+}
